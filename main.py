a655f56241b2a0a7982b50ea1566c5d98680cd1d
QuyXuan <maitrinhxuanquy@gmail.com>
2024-05-07 05:04:16 +0700

    refactor: Update captcha generation and prediction logic

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import customtkinter as ctk
 import tkinter as tk
-from tkinter import PhotoImage
-from tkinter import filedialog
-from solve import generate_captcha, load_model, predict_captcha
+from tkinter import filedialog, PhotoImage
+from solve import load_model, predict_list_captcha, generate_captcha, predict_captcha
 import os
 from PIL import Image, ImageTk
 
@@ -11,11 +10,12 @@ ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("dark-blue")
 
 root = ctk.CTk()
-root.geometry("800x500")
+root.geometry("1000x700")
 root.title("Captcha Solver")
 
 global model
 model = load_model()
+max_images_per_row = 5
 
 
 def on_closing():
@@ -27,71 +27,141 @@ def on_closing():
 root.protocol("WM_DELETE_WINDOW", on_closing)
 
 
-def update_image(label, button, image_path):
-    if os.path.exists(image_path):
-        # photo = PhotoImage(file=image_path)
-        # label.configure(image=photo)
-        # label.image = photo
-        image = Image.open(image_path)
-        photo = ImageTk.PhotoImage(image)
-        label.configure(image=photo)
-        label.image = photo
-
-        button.pack(pady=12, padx=10)
-        button.configure(command=lambda: predict(image_path))
-        entry2.pack(pady=12, padx=10)
-
-
-def open_file():
-    file_path = filedialog.askopenfilename(
-        initialdir="/", title="Select file", filetypes=(("jpg files", "*.jpg"),)
-    )
-    if file_path:
-        update_image(img_label, button_predict, file_path)
-
-
 def generate():
-    captcha_text = entry1.get()
+    captcha_text = textbox_captcha.get()
     generate_captcha(captcha_text)
-    update_image(img_label, button_predict, "captcha_out.png")
+    update_image("captcha_out.png")
 
 
 def predict(file_path):
     captcha_text = predict_captcha(model, file_path)
-    entry2.delete(0, "end")
-    entry2.insert(0, captcha_text)
+    textbox_predict.delete(0, "end")
+    textbox_predict.insert(0, captcha_text)
+
+
+def update_image(image_path):
+    if os.path.exists(image_path):
+        image = Image.open(image_path)
+        photo = ImageTk.PhotoImage(image)
+        label_image.configure(image=photo)
+        label_image.image = photo
+
+        button_predict.pack(pady=12, padx=10)
+        button_predict.configure(command=lambda: predict(image_path))
+        textbox_predict.pack(pady=12, padx=10)
+
+
+def update_flow_panel(file_paths, list_predictions=None):
+    for widget in flow_frame.winfo_children():
+        widget.destroy()
+
+    row = 0
+    column = 0
+    if list_predictions is None:
+        for file_path in file_paths:
+            if column >= max_images_per_row:
+                row += 1
+                column = 0
+
+            image = Image.open(file_path)
+            photo = ImageTk.PhotoImage(image)
+            label = ctk.CTkLabel(master=flow_frame, text="", image=photo)
+            label.image = photo
+            label.grid(row=row, column=column, padx=10, pady=10)
+            column += 1
+        flow_frame.update_idletasks()
+        canvas.pack(padx=90, pady=10, fill="both")
+        canvas.config(scrollregion=canvas.bbox("all"))
+        button_predict.pack(pady=12, padx=10)
+        button_predict.configure(command=lambda: handle_prediction(file_paths))
+    else:
+        for obj in list_predictions:
+            if column >= max_images_per_row:
+                row += 2
+                column = 0
+
+            image = Image.open(obj["file_path"])
+            photo = ImageTk.PhotoImage(image)
+            label_text = ctk.CTkLabel(master=flow_frame, text=obj["predicted_label"])
+            label_text.grid(row=row, column=column, padx=10, pady=5)
+            label_image = ctk.CTkLabel(master=flow_frame, text="", image=photo)
+            label_image.image = photo
+            label_image.grid(row=row + 1, column=column, padx=10, pady=(0, 5))
+            column += 1
+    flow_frame.update_idletasks()
+    canvas.pack(padx=90, pady=10, fill="both")
+    canvas.config(scrollregion=canvas.bbox("all"))
+
+
+def open_file():
+    file_paths = filedialog.askopenfilenames(
+        initialdir="/",
+        title="Select file",
+        filetypes=(("image files", "*.jpg *.jpeg *.png"),),
+    )
+    if file_paths:
+        update_flow_panel(file_paths)
+
+
+def update_canvas_scrollregion(event):
+    canvas.configure(scrollregion=canvas.bbox("all"))
+
+
+def handle_prediction(file_paths):
+    results = predict_list_captcha(model, file_paths)
+    for result in results:
+        print(
+            f"File: {result['file_path']}, Predicted Label: {result['predicted_label']}"
+        )
+    update_flow_panel(file_paths, results)
 
 
 frame = ctk.CTkFrame(master=root)
 frame.pack(pady=20, padx=60, fill="both", expand=True)
+frame.pack_propagate(False)
 
-label = ctk.CTkLabel(master=frame, text="Generate Captcha", font=("Roboto", 24))
-label.pack(pady=12, padx=10)
-
-entry1 = ctk.CTkEntry(
-    master=frame, placeholder_text="Enter your captcha", font=("Roboto", 12)
-)
-# entry1.pack(pady=12, padx=10)
-entry1.pack_forget()
-
-button_generate = ctk.CTkButton(
-    master=frame, text="Generate", font=("Roboto", 12), command=generate
-)
-# button_generate.pack(pady=12, padx=10)
-button_generate.pack_forget()
+label_title = ctk.CTkLabel(master=frame, text="Captcha Solver", font=("Roboto", 24))
+label_title.pack(pady=12, padx=10)
 
 button_upload = ctk.CTkButton(
     master=frame, text="Upload", font=("Roboto", 12), command=open_file
 )
-button_upload.pack(pady=12, padx=10)
+# button_upload.pack(pady=12, padx=10)
+button_upload.pack_forget()
 
-img_label = ctk.CTkLabel(master=frame, text="")
-img_label.pack(pady=12, padx=10)
+textbox_captcha = ctk.CTkEntry(
+    master=frame, placeholder_text="Enter your captcha", font=("Roboto", 12)
+)
+textbox_captcha.pack(pady=12, padx=10)
+
+button_generate = ctk.CTkButton(
+    master=frame, text="Generate", font=("Roboto", 12), command=generate
+)
+button_generate.pack(pady=12, padx=10)
+
+label_image = ctk.CTkLabel(master=frame, text="")
+label_image.pack(pady=12, padx=10)
+
+canvas = ctk.CTkCanvas(master=frame, width=900, height=300, bg="#212121")
+scrollbar = ctk.CTkScrollbar(master=canvas, command=canvas.yview)
+canvas.configure(yscrollcommand=scrollbar.set)
+canvas.pack_forget()
+scrollbar.pack(side="right", fill="y")
+
+flow_frame = ctk.CTkFrame(canvas, width=canvas.winfo_width(), height=200)
+flow_frame.bind("<Configure>", update_canvas_scrollregion)
+canvas.create_window((0, 0), window=flow_frame, anchor="nw")
+
 
 button_predict = ctk.CTkButton(master=frame, text="Predict", font=("Roboto", 12))
 button_predict.pack_forget()
 
-entry2 = ctk.CTkEntry(master=frame, font=("Roboto", 12))
-entry2.pack_forget()
+textbox_predict = ctk.CTkEntry(master=frame, font=("Roboto", 12))
+textbox_predict.pack_forget()
 
 root.mainloop()
+
+# Nếu muốn chạy chức năng predict nhiều file cùng 1 lúc
+# thì MỞ COMMENT dòng 129 và COMMENT dòng 130 -> 143
+# Và ngược lại nếu như muốn sử dụng chức năng generate và predict
+# thì COMMENT dòng 129 và MỞ COMMENT dòng 130 -> 143
--- solve.py
+++ solve.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 from tensorflow.keras.preprocessing import image as kimage
 import numpy as np
 
-CHARACTERS = [
+CHARACTERS_FULL = [
     "1",
     "2",
     "3",
@@ -65,14 +65,47 @@ CHARACTERS = [
     "y",
     "z",
 ]
+
+CHARACTERS_NUMBER = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
 CHAR_PER_LABEL = 5
 
 
-def generate_captcha(text):
-    image = ImageCaptcha(width=200, height=100, font_sizes=(50, 60, 70))
-    captcha_text = text
-    data = image.generate(captcha_text)
-    image.write(captcha_text, "captcha_out.png")
+class CleanCaptcha(ImageCaptcha):
+
+    def create_captcha_image(self, chars, color, background):
+        """Tạo hình ảnh CAPTCHA mà không có đường kẻ hay nhiễu."""
+
+        image = super().create_captcha_image(chars, color, background)
+        return image
+
+    def create_noise_curve(self, image, color):
+        """Bỏ qua việc tạo đường cong nhiễu."""
+        return image
+
+    def create_noise_dots(self, image, color, number=100):
+        """Bỏ qua việc tạo chấm nhiễu."""
+        return image
+
+
+font_paths = [r"E:\CaptchaSolver\ARIAL.TTF"]
+
+
+def generate_captcha(captcha_text):
+    captcha = CleanCaptcha(
+        width=140, height=50, fonts=font_paths, font_sizes=(30, 32, 34)
+    )
+    data = captcha.generate(captcha_text)
+    captcha.write(captcha_text, "captcha_out.png")
+
+
+def predict_captcha(model, file_path):
+    img = kimage.load_img(file_path, color_mode="grayscale", target_size=(40, 150))
+    img_array = kimage.img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0)
+    img_array = img_array / 255.0
+    y_pred = model.predict(img_array)
+    return one_hot_to_label(y_pred.squeeze())
 
 
 def load_model():
@@ -98,37 +131,45 @@ def load_model():
             tf.keras.layers.Dense(512, activation="relu"),
             tf.keras.layers.BatchNormalization(),
             tf.keras.layers.Dropout(0.75),
-            tf.keras.layers.Dense(len(CHARACTERS) * 5),
+            tf.keras.layers.Dense(len(CHARACTERS_NUMBER) * 5),
         ]
     )
-    model.load_weights("./captcha_solver_model.weights.h5")
+    model.load_weights("./captcha_solver_number_model.weights.h5")
     return model
 
 
-def predict_captcha(model, file_path):
-    img = kimage.load_img(file_path, color_mode="grayscale", target_size=(40, 150))
-    img_array = kimage.img_to_array(img)
-    img_array = np.expand_dims(img_array, axis=0)  # Thêm một chiều để tạo batch
-    img_array = img_array / 255.0  # Chuẩn hóa
-    y_pred = model.predict(img_array)
-    return one_hot_to_label(y_pred.squeeze())
+def predict_list_captcha(model, file_paths):
+    results = []
+    for file_path in file_paths:
+        img = kimage.load_img(file_path, color_mode="grayscale", target_size=(40, 150))
+        img_array = kimage.img_to_array(img)
+        img_array = np.expand_dims(img_array, axis=0)
+        img_array = img_array / 255.0
+        y_pred = model.predict(img_array)
+        results.append(
+            {
+                "file_path": file_path,
+                "predicted_label": one_hot_to_label(y_pred.squeeze()),
+            }
+        )
+    return results
 
 
 def one_hot_to_char(x: np.array):
     y = np.array(x)
     y = y.squeeze()
-    assert len(y) == len(CHARACTERS)
+    assert len(y) == len(CHARACTERS_NUMBER)
     idx = np.argmax(y)
-    return CHARACTERS[idx]
+    return CHARACTERS_NUMBER[idx]
 
 
 def one_hot_to_label(x):
     y = np.array(x)
     y = y.squeeze()
     label_list = []
-    assert len(y) == len(CHARACTERS * CHAR_PER_LABEL)
+    assert len(y) == len(CHARACTERS_NUMBER * CHAR_PER_LABEL)
     for i in range(0, CHAR_PER_LABEL):
-        start = i * len(CHARACTERS)
-        end = start + len(CHARACTERS)
+        start = i * len(CHARACTERS_NUMBER)
+        end = start + len(CHARACTERS_NUMBER)
         label_list.append(one_hot_to_char(y[start:end]))
     return "".join(label_list)
